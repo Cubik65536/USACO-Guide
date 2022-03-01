e84876ad321548a6430ad8e62d219d54e2bce3ce
Cubik65536 <cubik65536@cubik65536.top>
2022-03-01 14:08:16 -0500

    Add Silver Class - Week 1 - Classwork - Problem: Static Range Sum

---
+++ CPI Silver Class/Week 1: Prefix Sums/Classwork/Problem: Static Range Sum/Main.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Kattio io = new Kattio();
+
+        int n = io.nextInt();
+        int q = io.nextInt();
+
+    int[] a = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            a[i] = io.nextInt();
+        }
+
+        long[] prefixSum = new long[n + 1];
+        for (int i = 1; i <= n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + a[i];
+        }
+
+        for (int i = 0; i < q; i++) {
+            int l = io.nextInt();
+            int r = io.nextInt();
+
+            long sum = prefixSum[r] - prefixSum[l];
+            io.println(sum);
+        }
+
+        io.close();
+    }
+}
+
+class Kattio extends PrintWriter {
+    private BufferedReader r;
+    private StringTokenizer st;
+
+    // standard input
+    public Kattio() {
+        this(System.in, System.out);
+    }
+
+    public Kattio(InputStream i, OutputStream o) {
+        super(o);
+        r = new BufferedReader(new InputStreamReader(i));
+    }
+
+    // USACO-style file input
+    public Kattio(String problemName) throws IOException {
+        super(new FileWriter(problemName + ".out"));
+        r = new BufferedReader(new FileReader(problemName + ".in"));
+    }
+
+    // read next line
+    public String readLine() {
+        try {
+            return r.readLine();
+        } catch (IOException e) {
+        }
+        return null;
+    }
+
+    // returns null if no more input
+    public String next() {
+        try {
+            while (st == null || !st.hasMoreTokens())
+                st = new StringTokenizer(r.readLine());
+            return st.nextToken();
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    public double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    public long nextLong() {
+        return Long.parseLong(next());
+    }
+}
