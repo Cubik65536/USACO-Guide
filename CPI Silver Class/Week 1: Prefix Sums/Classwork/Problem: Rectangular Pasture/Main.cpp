a1402e196cdb3d33f98efb35acc1311db7419e1e
Cubik65536 <cubik65536@cubik65536.top>
2022-03-01 16:16:29 -0500

    Add Silver Class - Week 1 - Classwork - Problem: Rectangular Pasture

---
+++ CPI Silver Class/Week 1: Prefix Sums/Classwork/Problem: Rectangular Pasture/Main.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAXN = 2505;
+
+using ll = long long;
+using pii = pair<int, int>;
+
+int pre[MAXN][MAXN];
+
+int getSum(int x1, int y1, int x2, int y2) {
+    return pre[x2][y2] - pre[x1 - 1][y2] - pre[x2][y1 - 1] + pre[x1 - 1][y1 - 1];
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    pii cowCoordinates[MAXN];
+
+    for(int i = 0; i < n; i++) {
+		cin >> cowCoordinates[i].first >> cowCoordinates[i].second;
+    }
+
+	for(int t = 0; t < 2; t++) {
+		sort(cowCoordinates, cowCoordinates + n);
+		for(int i = 0; i < n; i++){
+			cowCoordinates[i].first = i + 1;
+			swap(cowCoordinates[i].first, cowCoordinates[i].second);
+		}
+	}
+
+	for(int i = 0; i < n; i++) {
+		pre[cowCoordinates[i].first][cowCoordinates[i].second]++;
+    }
+
+	for(int i = 1; i <= n; i++) {
+		for(int j = 1; j <= n; j++) {
+			pre[i][j] += pre[i - 1][j] + pre[i][j - 1] - pre[i - 1][j - 1];
+        }
+    }
+
+	ll result = 1;
+	for(int i = 0; i < n; i++){
+		for(int j = i; j < n; j++){
+			int x1 = min(cowCoordinates[i].first, cowCoordinates[j].first);
+			int x2 = max(cowCoordinates[i].first, cowCoordinates[j].first);
+			result += getSum(1, i + 1, x1, j + 1) * getSum(x2, i + 1, n, j + 1);
+		}
+	}
+
+    cout << result << endl;
+
+    return 0;
+
+}
