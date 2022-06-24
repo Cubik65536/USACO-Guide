53619e746f65facdaac5122a326f29754e0311a1
Cubik65536 <cubik65536@cubik65536.top>
2022-06-24 15:59:37 -0400

    Add Gold - Divisibility - Problem: Sum of Divisors

---
+++ Gold/Divisibility/Problem: Sum of Divisors/Main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+const int MOD = 1e9+7;
+
+int main() {
+    ll n;
+    cin >> n;
+
+    int result = 0;
+    for (ll i = 1, j; i <= n; i = j) {
+        ll q = n / i;
+        j = n / q + 1;
+        ll x = j - i;
+        ll y = i + j - 1;
+        if (x % 2 == 0) {
+            x /= 2;
+        } else {
+            y /= 2;
+        }
+        x %= MOD;
+        y %= MOD;
+
+        result = (result + q % MOD * x % MOD * y % MOD) % MOD;
+    }
+    
+    cout << result << endl;
+
+    return 0;
+
+}
